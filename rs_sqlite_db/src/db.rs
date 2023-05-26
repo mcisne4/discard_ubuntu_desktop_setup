981037fbaa0deeb3abef23924c78dc3c1344d959
mcisne4 <mcisne4@gmail.com>
2023-05-26 01:02:33 -0500

    rs_sqlite_db: Add db::check_table_row_count

--- rs_sqlite_db/src/db.rs
+++ rs_sqlite_db/src/db.rs
@@ -6,3 +6,6 @@ pub use table_exists::table_exists;
 
 mod check_table_columns;
 pub use check_table_columns::check_table_columns;
+
+mod check_table_row_count;
+pub use check_table_row_count::check_table_row_count;
--- rs_sqlite_db/src/lib.rs
+++ rs_sqlite_db/src/lib.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 mod db;
-use db::{check_table_columns, connect, table_exists};
+use db::{check_table_columns, check_table_row_count, connect, table_exists};
 
 pub fn hello() {
     println!("Hello from 'rs_sqlite_db'");
@@ -17,6 +17,11 @@ pub fn init(db_dir: PathBuf) -> Result<(), String> {
     if table_exists {
         let columns_are_valid = check_table_columns(&conn)?;
         println!("COLUMNS ARE VALID: {}", columns_are_valid);
+
+        if columns_are_valid {
+            let row_count_is_valid = check_table_row_count(&conn)?;
+            println!("ROWS COUNT IS VALID: {}", row_count_is_valid);
+        }
     }
 
     Ok(())
