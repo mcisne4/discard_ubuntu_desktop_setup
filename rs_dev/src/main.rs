f8f09f61091d464a03e5f30e615dcaa55d520aa4
mcisne4 <mcisne4@gmail.com>
2023-06-02 17:34:34 -0500

    rs_dev: Add dev states for rs_logger

--- rs_dev/src/main.rs
+++ rs_dev/src/main.rs
@@ -1,12 +1,15 @@
 mod mods;
 mod util;
 
+pub use mods::rs_logger::{dev_rs_logger, LoggerMode};
+
 fn main() {
     println!("=======================================");
     println!("========= Hello from 'rs_dev' =========");
     println!("=======================================\n");
 
-    mods::rs_logger::dev(true);
+    dev_rs_logger(LoggerMode::Init);
+
     mods::rs_shell::dev(false);
     mods::rs_sqlite_db::dev(false);
 }
--- rs_dev/src/mods/rs_logger.rs
+++ rs_dev/src/mods/rs_logger.rs
@@ -14,66 +14,32 @@ use logs_standard::starndard_logs;
 mod logs_err_conversion;
 use logs_err_conversion::log_error_conversions;
 
-pub fn dev(run: bool) {
-    if !run {
-        return;
-    }
+mod initialize;
+use initialize::initialize;
 
-    println!("--------------------------------------");
-    println!("--- Developing for Crate: 'rs_log' ---");
-    println!("--------------------------------------\n");
-
-    // --- Startup Initialization --- //
-    startup_functions();
-
-    // --- Standard Logs --- //
-    starndard_logs();
-
-    // --- Logging and Converting Errors --- //
-    log_error_conversions();
-
-    // // --- Writting Tests --- //
-    // let source = "rs_dev|rs_logs";
-
-    // // ___ Log: INFO ___ //
-    // let msg = "An info statement";
-    // println!("Push an 'INFO' log: '{}'\n", msg);
-    // log_info(&source, msg);
-
-    // // ___ Log: WARNING ___ //
-    // let msg = "A warning statement";
-    // println!("Push a 'WARN' log: '{}'\n", msg);
-    // log_warn(&source, msg);
-
-    // // ___ Log: ERROR ___ //
-    // let cause = "The cause of the error";
-    // let description = "A warning statement";
-    // println!(
-    //     "Push an 'ERROR' log:\n  cause: '{}'\n  description: '{}'\n",
-    //     cause, description
-    // );
-    // log_error(&source, cause, description);
-
-    // // ___ Log: ERROR map_err ___ //
-    // let cause = "Error converted with 'log_map_err()'";
-    // let response = "Intercepted error response";
-    // match failing_fn().map_err(|e| {
-    //     println!(
-    //         "Push an 'ERROR' log from 'map_err' conversion:\n  cause: '{}'\n  description: '{}'",
-    //         cause,
-    //         e.to_string()
-    //     );
-    //     log_map_err(&source, cause, e, Some(response))
-    // }) {
-    //     Ok(_) => (),
-    //     Err(e) => println!("  result: '{}'\n", e),
-    // }
+pub enum LoggerMode {
+    FullDev,
+    Init,
+    DontRun,
 }
 
-// fn failing_fn() -> Result<(), LogError> {
-//     let fail = true;
-//     match fail {
-//         true => Err(LogError::AnError(String::from("forced fail function"))),
-//         false => Ok(()),
-//     }
-// }
+pub fn dev_rs_logger(mode: LoggerMode) {
+    match mode {
+        LoggerMode::DontRun => return,
+        LoggerMode::FullDev => {
+            println!("--------------------------------------");
+            println!("--- Developing for Crate: 'rs_log' ---");
+            println!("--------------------------------------\n");
+
+            // --- Startup Initialization --- //
+            startup_functions();
+
+            // --- Standard Logs --- //
+            starndard_logs();
+
+            // --- Logging and Converting Errors --- //
+            log_error_conversions();
+        }
+        LoggerMode::Init => initialize(),
+    }
+}
