5e719dfb6c7dca39bd7f549e707d9e7eee582d64
mcisne4 <mcisne4@gmail.com>
2023-06-04 15:35:03 -0500

    rs_shell: Integrate rs_logger

--- rs_shell/src/env/shell.rs
+++ rs_shell/src/env/shell.rs
@@ -1,8 +1,17 @@
+use rs_logger::logging::{log_info, log_map_err};
 use std::env;
 
 pub fn shell() -> Result<String, String> {
+    let source = "rs_shell|env|shell";
+    let err_msg = "Unable to retrieve the environmental 'SHELL' variable";
+
     let key = "SHELL";
-    let mut shell = env::var(key).map_err(|e| format!("$SHELL: {}", e))?;
+    let mut shell = env::var(key).map_err(|e| log_map_err(&source, err_msg, e, None))?;
+
+    log_info(
+        &source,
+        &format!("Retrieved env 'SHELL' variable: {}", &shell),
+    );
 
     if shell.ends_with("bash") {
         shell = String::from("BASH");
--- rs_shell/src/lib.rs
+++ rs_shell/src/lib.rs
@@ -3,7 +3,3 @@ pub mod types;
 pub mod env;
 pub mod is_installed;
 pub mod shell;
-
-pub fn hello() {
-    println!("Hello from 'rs_shell'");
-}
